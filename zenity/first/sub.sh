f3c53a29681bc7523b27b76531179a97c4098ca9
Dzmitry Samsonau <demn_@tut.by>
2015-04-12 10:01:28 +0200

    Added substitution file

---
+++ zenity/first/sub.sh
@@ -0,0 +1,8 @@
+num1=$(zenity --entry --title='substitution' --text='enter first number')
+echo $num1
+echo '-'
+num2=$(zenity --entry --title='substitution' --text="enter second number")
+echo $num2
+echo '='
+echo $(($num1 - $num2))
+
