7dd1c2a0df18ba8df359b73012521c71e293522f
Dzmitry Samsonau <demn_@tut.by>
2015-04-12 09:41:26 +0200

    Created zenity file for addition

--- zenity/first/add.sh
+++ zenity/first/add.sh
@@ -1,1 +1,8 @@
- sh -c 'zenity --info --text "$(uptime)"' 
+num1=$(zenity --entry --title='addition' --text='enter first number')
+echo $num1
+echo '+'
+num2=$(zenity --entry --title='addition' --text="enter second number")
+echo $num2
+echo '='
+echo $(($num1 + $num2))
+
