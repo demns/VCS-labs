53b3e787758d1ed241252f57bd4b0ff899e43789
Dzmitry Samsonau <demn_@tut.by>
2015-04-21 15:22:01 +0200

    Added conjunction

--- zenity/third/main.sh
+++ zenity/third/main.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-operation=$(zenity --list --text "Select an operation" --radiolist --column "Pick" --column "Operation" --column "File" TRUE add addition FALSE sub subtraction FALSE mul multiplication FALSE div division FALSE mod modulo FALSE neg negation)'.sh'; 
+operation=$(zenity --list --text "Select an operation" --radiolist --column "Pick" --column "Operation" --column "File" TRUE add addition FALSE sub subtraction FALSE mul multiplication FALSE div division FALSE mod modulo FALSE neg negation FALSE con conjunction)'.sh'; 
 
 sh $operation
 
--- zenity/third/neg.sh
+++ zenity/third/neg.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-num=$(zenity --entry --title='negation' --text='enter first number')
+num=$(zenity --entry --title='negation' --text='enter zero or anything else')
 echo '!'
 echo $num
 echo $((! $num))
