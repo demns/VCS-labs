0cb61b40af190fca7707da72de85a876eabfed0b
Dzmitry Samsonau <demn_@tut.by>
2015-04-21 17:12:03 +0200

    Recreated sqrt (without bc now)

--- zenity/third/sqrt.sh
+++ zenity/third/sqrt.sh
@@ -3,5 +3,15 @@
 num=$(zenity --entry --title='square root' --text='enter number')
 echo 'sqrt'
 echo $num
-echo "scale=4;sqrt($num)" | bc
+echo '='
+
+prev=100;
+b=10;
+
+while [ $(($prev-$b)) -ge 1 ]; do
+  prev=$b;
+  b=$((($b+$num/$b)/2));
+done
+
+echo $b
 
